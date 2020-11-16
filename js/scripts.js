b321c7ebdf87c42e3fbace33719172a3f5e974e9
saurabh-saurabh <saurabhprajapati120@gmail.com>
2020-11-16 06:27:27 +0530

    F C

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function check(){
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if(username == 'lol' && password == 'lol') {
+        window.location.href = "index.html"
+    }
+}
+
+
